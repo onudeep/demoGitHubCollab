550c5c88d4d321f6f921bfa88ad566582c48d182
ashishs <ashishs@nrifintech.com>
2017-09-21 13:23:24 +0530

    Commit Message test

--- DateTimeDemo/Program.cs
+++ DateTimeDemo/Program.cs
@@ -12,7 +12,7 @@ namespace DateTimeDemo
         {
             DateTime dateTime = DateTime.Now;
             DateTime date1 = new DateTime();
-            DateTime date2 = DateTime.Parse("nov-02-1993");
+            DateTime date2 = DateTime.Parse("july-01-1995");
             Console.WriteLine("ToString() " + date2.ToLongDateString()); 
             Console.WriteLine("ToShortDateString() " + dateTime.ToShortDateString());
             Console.ReadLine();            
