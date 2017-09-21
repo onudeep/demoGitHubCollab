9735c0268c867ebf1244b2ad5356b0d792cfd549
saurabha <saurabha@nrifintech.com>
2017-09-21 14:23:40 +0530

    AddDays method used

--- DateTimeDemo/Program.cs
+++ DateTimeDemo/Program.cs
@@ -16,6 +16,8 @@ namespace DateTimeDemo
             Console.WriteLine("ToString() " + date2.ToLongDateString()); 
             Console.WriteLine("ToShortDateString() " + dateTime.ToShortDateString());
             Console.WriteLine("Testing...");
+            DateTime days= DateTime.Now.AddDays(10);
+            Console.WriteLine("10 days added "+ days);
             Console.ReadLine();            
         }
         
